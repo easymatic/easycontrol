baf0651917ce60f3b911c40d611926a0a08b186d
Aborilov Pavel <aborilov@gmail.com>
2017-10-07 19:43:06 +0300

    add command delay

--- handler/actionhandler/actionhandler.go
+++ handler/actionhandler/actionhandler.go
@@ -3,50 +3,58 @@ package actionhandler
 import (
 	"fmt"
 	"io/ioutil"
+	"time"
 
 	yaml "gopkg.in/yaml.v2"
 
 	"github.com/easymatic/easycontrol/handler"
 )
 
-type Actions struct {
+const (
+	COMMAND_SET   = "set"
+	COMMAND_DELAY = "delay"
+)
+
+type Config struct {
 	Actions []struct {
 		Name     string        `yaml:"name"`
 		Event    handler.Event `yaml:"event"`
 		Commands []struct {
 			Name    string          `yaml:"name"`
 			Command handler.Command `yaml:"command"`
+			Params  interface{}     `yaml:"params"`
 		} `yaml:"commands"`
 	} `yaml:"actions"`
 }
 
-func getActionsConfig() Actions {
-	var actions Actions
+func getActionsConfig() Config {
+	var config Config
 	yamlFile, err := ioutil.ReadFile("config/actions.yaml")
 	if err != nil {
 		panic(err)
 	}
-	err = yaml.Unmarshal(yamlFile, &actions)
+	err = yaml.Unmarshal(yamlFile, &config)
 	if err != nil {
 		panic(err)
 	}
-	return actions
+	return config
 }
 
 type ActionHandler struct {
 	handler.BaseHandler
-	Actions Actions
+	Config Config
 }
 
 func NewActionHandler() *ActionHandler {
 	rv := &ActionHandler{}
 	rv.Init()
 	rv.Name = "actionhandler"
-	rv.Actions = getActionsConfig()
+	rv.Config = getActionsConfig()
 	return rv
 }
 
 func (hndl *ActionHandler) Start() error {
+	fmt.Printf("actions: %v\n", hndl.Config)
 	hndl.BaseHandler.Start()
 	hndl.EventReader = hndl.Broadcaster.Listen()
 
@@ -54,10 +62,14 @@ func (hndl *ActionHandler) Start() error {
 		select {
 		case e := <-hndl.EventReader.Ch:
 			event := e.(handler.Event)
-			for _, action := range hndl.Actions.Actions {
+			for _, action := range hndl.Config.Actions {
 				if action.Event == event {
 					for _, command := range action.Commands {
-						hndl.SetTag(command.Command)
+						if command.Name == COMMAND_SET {
+							hndl.SetTag(command.Command)
+						} else if command.Name == COMMAND_DELAY {
+							time.Sleep(time.Second * time.Duration(command.Params.(int)))
+						}
 					}
 				}
 			}
